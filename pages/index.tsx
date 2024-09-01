9e8a3ed2e604f0e00346d8f4741d5a8f52389327
nishio.n0522 <nishio.n0522@gmail.com>
2024-09-01 17:46:01 +0900

    test_call_api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,58 @@
 import Image from "next/image";
 
-import { Box, Stack, Typography } from "@mui/material";
+import { fetchAuthSession } from "aws-amplify/auth";
+
+import { Box, Button, Stack, Typography } from "@mui/material";
 
 import homeRabbitImg from "@/public/onigiriRabbitImg.png";
 import { grey } from "@mui/material/colors";
 
 import "@aws-amplify/ui-react/styles.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [text, setText] = useState<string>("None");
+
+  const handleClick = () => {
+    const data = {
+      key1: "value1",
+      key2: "value2",
+    };
+
+    axios
+      .get("https://niginigi-onigiri.studio/api/test")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => console.log("err", err));
+
+    // const callApi = (token: string) =>
+    //   axios
+    //     .post(
+    //       "https://h6nad1f1qi.execute-api.ap-northeast-1.amazonaws.com/testing/api/v1/create-summary",
+    //       data,
+    //       {
+    //         withCredentials: true,
+    //         headers: {
+    //           "Content-Type": "application/json",
+    //           Authorization: `Bearer ${token}`,
+    //         },
+    //       }
+    //     )
+    //     .then((response) => {
+    //       console.log(response);
+    //     })
+    //     .catch((err) => console.log("err", err));
+
+    // fetchAuthSession().then((res) => {
+    //   console.log("nanikore", res);
+    //   if (!res.tokens) return;
+    //   console.log(res.tokens.idToken?.toString());
+    //   callApi(res.tokens.accessToken.toString());
+    // });
+  };
+
   return (
     <Stack
       direction="column"
@@ -47,6 +92,10 @@ export default function App() {
           This app is a playground for hobbyists to create and try out things in
           their spare time.
         </Typography>
+        <Button variant="contained" color="primary" onClick={handleClick}>
+          test button
+        </Button>
+        <Typography color="white">{text}</Typography>
       </Stack>
     </Stack>
   );
