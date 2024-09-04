5f722a5af90a31dc8e426e3fe82ecdacbfc51bc2
nishio.n0522 <nishio.n0522@gmail.com>
2024-09-04 21:01:24 +0900

    completed PoC for connecting amplify and api gateway, lambda

--- api/src/main.py
+++ api/src/main.py
@@ -45,6 +45,13 @@ def lambda_handler(event, context):
 
     return {
         "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "http://localhost:3000",
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Methods": "DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT",
+            "Access-Control-Allow-Credentials": True
+        },
         "body": json.dumps({
             "message": response.choices[0].message.content
         }, ensure_ascii=False),
