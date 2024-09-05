419757355df559d921402ff31e68ad440d65ccd0
nishio.n0522 <nishio.n0522@gmail.com>
2024-09-05 22:24:35 +0900

    completed sqs+lambda trial

--- api/src/async_processing.py
+++ api/src/async_processing.py
@@ -1,6 +1,12 @@
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
+
+    logger.info("execute async processing lambda!!!")
 
     return {
         "statusCode": 200,
